d7093c1990806518ce8fc7c75017a365cb7befcb
mrrcrick@gmail.com <mrrcrick@gmail.com>
2017-05-15 18:42:20 +0100

    add blah

--- demo DEPLOY/app/Http/routes.php
+++
@@ -1,3 +0,0 @@
-<?php
-Route::get('test/',function(){ return 'we here';});
-Route::get('testroute/',function(){ return 'we here';});
--- demo DEPLOY/resources/views/deletethis/info.blade.php
+++
@@ -1,7 +0,0 @@
-
-@section('content')
-    <div >
-        This is a rendered page
-		<p>blah blah
-    </div>
-@stop
